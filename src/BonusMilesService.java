51847cf2c6a2b829726f9c661041aa64c8ac689a
Ekaterina <vipolsovatel@mail.ru>
2022-05-09 15:30:52 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = 10_000;
+        int bonus = 20;
+        cost = price/bonus;
+        return cost;
+    }
+}
