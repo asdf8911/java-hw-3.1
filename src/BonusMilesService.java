f73ef50871e021ec22cf61847f5993c4f436a313
Ekaterina <vipolsovatel@mail.ru>
2022-05-15 13:52:48 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int price = 10_000;
         int bonus = 20;
-        cost = price/bonus;
+        cost = price / bonus;
         return cost;
     }
 }
